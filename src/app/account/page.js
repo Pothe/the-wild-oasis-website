72f37203181753910482dafabd5c656184b2b86c
Pothe <keoutpothe2014@gmail.com>
2026-03-30 15:10:41 +0700

    the wild oasis website

--- src/app/account/page.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function page() {
-    return <h1>account the wild oasis </h1>
-    
-}
--- src/app/cabins/page.js
+++
@@ -1,5 +0,0 @@
-
-
-export default function page() {
-    return <h1>all cabins in here</h1>
-}
